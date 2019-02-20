3533ea886d31153ee0f5592a07365d31f5a97eb6
ss1.h18@nemandi.ntv.is <ss1.h18@nemandi.ntv.is>
2019-02-20 14:10:43 +0000

    Implementing Module Patterns

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,39 @@
+// Budget Module
+var budgetController = (function() {
+
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+
+})();
+
+
+// UI Module
+var UIController = (function() {
+
+    // Some code
+
+})();
+
+
+// App Module
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var z = budgetCtrl.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
+
